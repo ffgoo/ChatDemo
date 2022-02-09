9a0143d0b963cb382f35655c1638d33bf557808b
lee <sylee@nutritioncourt.co,m>
2022-02-09 14:41:01 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+import socketserver
+import threading
+
+HOST = '127.0.0.1'
+PORT = 9012
+lock = threading.Lock()
+
+
+class UserManager:
+    def __init__(self):
+        self.users = {}
+
+    def addUser(self, username, conn, addr):  # 사용자 ID를 self.users에 추가하는 함수
+        if username in self.users:  # 이미 등록된 사용자라면
+            conn.send('이미 등록된 사용자입니다.\n'.encode())
+            return None
+
+
+
+        lock.acquire()
+        self.users[username] = (conn, addr)
+        lock.release()
+
+        self.sendMessageToAll('[%s]님이 입장했습니다.' % username)
+        print('+++ 대화 참여자수 [%d]' % len(self.users))
+
+    def removeUser(self, username):
+        if username not in self.users:
+            return
+        lock.acquire()
+        del self.users[username]
+        lock.release()
+
+        self.sendMessageToAll('[%s]님이 퇴장하셨습니다.' % username)
+        print('---대화 참여자 수 [%d]' % len(self.users))
+
+    def messageHandler(self, username, msg):
+        if msg[0] != '/':
+            self.sendMessageToAll('[%s]%s' % (username, msg))
+            return
+        if msg.strip() == '/quit':
+            self.removerUser(username)
+            return -1
+
+    def sendMessageToAll(self, msg):
+        for conn, addr in self.users.values():
+            conn.send(msg.encode())
+
+
+class MyTcpHandler(socketserver.BaseRequestHandler):
+    userman = UserManager()
+
+    def handle(self):
+        print('[%s] 연결됨' % self.client_address[0])
+
+        try:
+            username = self.registerUsername()
+            msg = self.request.recv(1024)
+            while msg:
+                print(msg.decode())
+                if self.userman.messageHandler(username, msg.decode()) == -1:
+                    self.request.close()
+                    break
+                msg = self.request.recv(1024)
+
+        except Exception as e:
+            print(e)
+
+        print('[%s] 접속종료' % self.client_address[0])
+
+        self.userman.removeUser(username)
+
+    def registerUsername(self):
+        while True:
+            self.request.send('로그인ID :'.encode())
+            username = self.request.recv(1024)
+            username = username.decode().strip()
+            if self.userman.addUsers(username, self.request, self.client_address):
+                return username
+
+
+class ChatingServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
+
+
+def runServer():
+    print('+++채팅서버를 시작합니다')
+    print('+++채팅서버를 끝내려면 Ctrl-c를 누르세요')
+
+    try:
+        server = ChatingServer((HOST, PORT), MyTcpHandler)
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print('---채팅 서버를 종료합니다')
+        server.shutdown()
+        server.server_close()
+
+runServer()
+
